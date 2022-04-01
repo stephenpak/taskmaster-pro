4656c56c8fae7e90ad090061ada5c78f125516f2
Stephen Pak <stephenjpak@gmail.com>
2022-04-01 16:54:54 -0700

    4-1-22-454

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,42 @@
-var tasks = {};
+var auditTask = function (taskEl) {
+  // get date from task element
+  var date = $(taskEl).find('span').text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, 'L').set('hour', 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass('list-group-item-warning list-group-item-danger');
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass('list-group-item-danger');
+  } else if (Math.abs(moment().diff(time, 'days')) <= 2) {
+    $(taskEl).addClass('list-group-item-warning');
+  }
+};
 
 var createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $('<li>').addClass('list-group-item');
+
   var taskSpan = $('<span>')
     .addClass('badge badge-primary badge-pill')
     .text(taskDate);
+
   var taskP = $('<p>').addClass('m-1').text(taskText);
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $('#list-' + taskList).append(taskLi);
 };
+
+var tasks = {};
 
 var loadTasks = function () {
   tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -228,5 +251,24 @@ $('#remove-tasks').on('click', function () {
 $('#modalDueDate').datepicker({
   minDate: 1,
 });
+
+$('.list-group').on('change', "input[type='text']", function () {
+  var date = $(this).val();
+
+  var status = $(this).closest('.list-group').attr('id').replace('list-', '');
+  var index = $(this).closest('.list-group-item').index();
+
+  tasks[status][index].date = date;
+  saveTasks();
+
+  var taskSpan = $('<span>')
+    .addClass('badge badge-primary badge-pill')
+    .text(date);
+  $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest('.list-group-item'));
+});
+
 // load tasks for the first time
 loadTasks();
